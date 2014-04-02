689e53a2a8f9d08dac51af40bf33c476b925092e
Simon Rai <simon@raize.ca>
2014-04-02 06:34:15 +0000

    add review model

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,17 +1,13 @@
 class Movie < ActiveRecord::Base
 
+  has_many :reviews
+
 	validates :title, presence: true
-
   validates :director, presence: true
-
   validates :runtime_in_minutes, numericality: { only_integer: true }
-
   validates :description, presence: true
-
   validates :poster_image_url, presence: true
-
   validates :release_date, presence: true
-
   validate :release_date_is_in_the_future
 
   protected
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,12 +2,11 @@ class User < ActiveRecord::Base
 
 	has_secure_password
 
+	has_many :reviews
+
   validates :email, presence: true
-
-  validates :firstname, presence: true
-
-  validates :lastname, presence: true
-
-  validates :password, length: { in: 6..20 }, on: :create
+	validates :firstname, presence: true
+	validates :lastname, presence: true
+	validates :password, length: { in: 6..20 }, on: :create
 
 end
