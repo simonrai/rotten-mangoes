5b3673236c32ea9f05d417703a8fd32505945e22
Simon Rai <simon@raize.ca>
2014-04-02 00:24:06 +0000

    add login functionality

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 RottenMangoes::Application.routes.draw do
 
+  get "sessions/new"
+  get "sessions/create"
   get "users/new"
   get "users/create"
-  
-  resources :movies
 
+  resources :movies
   resources :users, only: [:new, :create]
+  resources :sessions, only: [:new, :create]
 
 end
