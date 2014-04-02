0f3cf2960c3df0af097b5ea5d02951b981bdf13d
Simon Rai <simon@raize.ca>
2014-04-02 00:49:07 +0000

    added footer with login/logout links

--- config/routes.rb
+++ config/routes.rb
@@ -8,5 +8,6 @@ RottenMangoes::Application.routes.draw do
   resources :movies
   resources :users, only: [:new, :create]
   resources :sessions, only: [:new, :create]
+  resources :sessions, only: [:new, :create, :destroy]
 
 end
